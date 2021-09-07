9a7545e92dd60863fcf1905eb8e737fa0729ee63
Melody-Ruth <melody@sd-fun.org>
2021-09-07 10:25:33 -0700

    working on implementing control variates paper

--- exper.cpp
+++ exper.cpp
@@ -3,6 +3,14 @@
 #include <random>
 #include <time.h>
 #include <math.h>
+#include <utility>
+#include <queue>
+
+using namespace std;
+
+double determinant(double a, double b, double c, double d, double e, double f, double g, double h, double i) {
+    return a * (e*i - h*f) - b * (d*i - g*f) + c * (d*h - g*e);
+}
 
 double foo(double x) {
     return 0.2 * sin(100*x) + 0.1 * sin(40*x) - 2 * (x - 0.6) * (x-0.6) + 0.15 * cos(30*x) + 1;
@@ -12,8 +20,16 @@ double pdf(double x) {
     return -3 * (x - 0.5) * (x - 0.5) + 1.25;
 }
 
+double pdf2(double x) {
+    return -1.786 * x * x + 2.099 * x + 0.546;
+}
+
 double cdf(double x) {
-    -1/8.0 - (x - 0.5) * (x - 0.5) * (x - 0.5) + 1.25 * x;
+    return -1/8.0 - (x - 0.5) * (x - 0.5) * (x - 0.5) + 1.25 * x;
+}
+
+double cdf2(double x) {
+    return -0.595 * x * x * x + 1.05 * x * x + 0.546 * x;
 }
 
 double invCdf(double x) {
@@ -35,6 +51,103 @@ double invCdf(double x) {
     return tempY;
 }
 
+double invCdf2(double x) {
+    double start = 0;
+    double end = 1.0;
+    double tempY = (start + end) / 2;
+    double tempX = cdf2(tempY);
+    double count = 0;
+    while (count < 250 && abs(tempX - x) > 0.0001) {
+        tempY = (start + end) / 2;
+        tempX = cdf2(tempY);
+        if (tempX > x) {
+            end = tempY;
+        } else {
+            start = tempY;
+        }
+        count ++;
+    }
+    return tempY;
+}
+
 double foo2(double x) {
     return foo(invCdf(x)) / pdf(invCdf(x));
+}
+
+double foo22(double x) {
+    return foo(invCdf2(x)) / pdf2(invCdf2(x));
+}
+
+const int regionsBudget = 7;
+const double epsilon = 0.00001;
+
+double miniSimpsons(double intervalStart, double intervalEnd, function<double (double)> testFunc) {
+    return (testFunc(intervalStart) + 4 * testFunc((intervalStart + intervalEnd) / 2.0) + testFunc(intervalEnd)) * (intervalEnd - intervalStart) / 6.0;
+}
+
+double miniTrapezoidal(double intervalStart, double intervalEnd, function<double (double)> testFunc) {
+    return (intervalEnd - intervalStart) * (testFunc(intervalEnd) + testFunc(intervalStart)) / 2.0;
+}
+
+double estimateError(double intervalStart, double intervalEnd, function<double (double)> testFunc) {
+    return abs(miniSimpsons(intervalStart, intervalEnd, testFunc) - miniTrapezoidal(intervalStart, intervalEnd, testFunc)) + epsilon * (intervalEnd - intervalStart);
+}
+
+void getCoefs(double x1, double x3, function<double (double)> testFunc) {
+    //quadratic approximation
+    double x2 = (x1 + x3) / 2.0;
+    double y1 = testFunc(x1);
+    double y2 = testFunc(x2);
+    double y3 = testFunc(x3);
+
+    double D = determinant(x1*x1, x1, 1, x2*x2, x2, 1, x3*x3, x3, 1);
+    double Da = determinant(y1, x1, 1, y2, x2, 1, y3, x3, 1);
+    double Db = determinant(x1*x1, y1, 1, x2*x2, y2, 1, x3*x3, y3, 1);
+    double Dc = determinant(x1*x1, x1, y1, x2*x2, x2, y2, x3*x3, x3, y3);
+    
+    double a = Da / D;
+    double b = Db / D;
+    double c = Dc / D;
+
+    cout << x1 << " to " << x3 << endl;
+    cout << a << ", " << b << ", " << c << endl;
+}
+
+double a = 0;
+double b = 1;
+
+void splitRegions(function<double (double)> testFunc) {
+    double regions[regionsBudget][2];
+
+    int numRegions = 1;
+    tuple<double, double, double> curr;
+    priority_queue<tuple<double, double, double>> myHeap;
+    //Each tuple has error estimate, startX, stopX
+    myHeap.push(tuple<double, double, double>(estimateError(a, b, testFunc),a,b));
+    double start, end, mid;
+    while (numRegions < regionsBudget) {
+        curr = myHeap.top();
+        start = get<1>(curr);
+        end = get<2>(curr);
+        mid = (start + end) / 2;
+        myHeap.pop();
+        myHeap.push(tuple<double, double, double>(estimateError(start, mid, testFunc), start, mid));
+        myHeap.push(tuple<double, double, double>(estimateError(mid, end, testFunc), mid, end));
+        numRegions++;
+    }
+
+    for (int i = 0; i < regionsBudget; i++) {
+        curr = myHeap.top();
+        regions[i][0] = get<1>(curr);
+        regions[i][1] = get<2>(curr);
+        myHeap.pop();
+        getCoefs(regions[i][0], regions[i][1], testFunc);
+        //cout << regions[i][0] << ", " << regions[i][1] << endl;
+    }
+}
+
+int main() {
+    //getCoefs(0, 1, foo);
+    //cout << determinant(2, 3, 7, -5, 23, 10, 4, 5, -1);
+    splitRegions(foo22);
 }
--- testSampleStrat.py
+++ testSampleStrat.py
@@ -9,10 +9,12 @@ def foo(x):
     return 0.2 * math.sin(100*x) + 0.1 * math.sin(40*x) - 2 * (x - 0.6) * (x-0.6) + 0.15 * math.cos(30*x) + 1
 
 def pdf(x):
-    return -3 * (x - 0.5) ** 2 + 1.25
+    #return -3 * (x - 0.5) ** 2 + 1.25
+    return -1.786 * x * x + 2.099 * x + 0.546
 
 def cdf(x):
-    return -1/8.0 - (x - 0.5) ** 3 + 1.25*x
+    #return -1/8.0 - (x - 0.5) ** 3 + 1.25*x
+    return -0.595 * x * x * x + 1.05 * x * x + 0.546 * x
 
 def invCdf(x):
     start = 0
@@ -54,6 +56,42 @@ plt.plot(x, y2)
 plt.subplot(1, 2, 2)
 y = foo2_vec(x)
 plt.plot(x, y)
+
+x = np.linspace(0.0, 0.25, 100)
+y = -2.08897 * x**2 - 0.560147 * x + 0.783074
+plt.plot(x, y, color='orange')
+plt.axvline(x=0.25, linestyle='--', color='red')
+
+x = np.linspace(0.25, 0.5, 100)
+y = -3.38676 * x**2 + 3.61422 * x + -0.169406
+plt.plot(x, y, color='orange')
+plt.axvline(x=0.5, linestyle='--', color='red')
+
+x = np.linspace(0.5, 0.625, 100)
+y = 8.45619 * x**2 + -6.32399 * x + 1.83896
+plt.plot(x, y, color='orange')
+plt.axvline(x=0.625, linestyle='--', color='red')
+
+x = np.linspace(0.625, 0.75, 100)
+y = -6.11312 * x**2 + 3.64454 * x + 1.29977
+plt.plot(x, y, color='orange')
+plt.axvline(x=0.75, linestyle='--', color='red')
+#plt.axvline(x=0.78125, linestyle='--', color='red')
+#plt.axvline(x=0.8125, linestyle='--', color='red')
+
+x = np.linspace(0.75, 0.875, 100)
+y = -50.6355 * x**2 + 83.3905 * x + -33.4658
+plt.plot(x, y, color='orange')
+plt.axvline(x=0.875, linestyle='--', color='red')
+
+x = np.linspace(0.875, 0.9375, 100)
+y = -317.799 * x**2 + 571.103 * x + -255.667
+plt.plot(x, y, color='orange')
+plt.axvline(x=0.9375, linestyle='--', color='red')
+
+x = np.linspace(0.9375, 1, 100)
+y = -360.013 * x**2 + 702.895 * x + -342.12
+plt.plot(x, y, color='orange')
 plt.show()
 
 # for x1 in np.arange(0.0,1.0,0.1):
